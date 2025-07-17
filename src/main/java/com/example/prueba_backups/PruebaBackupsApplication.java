adfe8ca5b73decee4ed3568c73137c157bd7bd20
Theo Pelegrina <theopelegrina469@gmail.com>
2025-07-17 20:53:24 -0300

    first commit

---
+++ src/main/java/com/example/prueba_backups/PruebaBackupsApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba_backups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBackupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBackupsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prueba_backups/PruebaBackupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_backups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBackupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
