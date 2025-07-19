7df94214a76561bbb576a36c22533b7beac0d7cd
tpelegrina <theopelegrina468@gmail.com>
2025-07-19 19:46:24 -0300

    probando backup en produccion - railway

--- src/main/java/com/example/prueba_backups/BackupController.java
+++ src/main/java/com/example/prueba_backups/BackupController.java
@@ -2,9 +2,11 @@ package com.example.prueba_backups;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "backups")
 public class BackupController {
     @Autowired
     private BackupService backupService;
