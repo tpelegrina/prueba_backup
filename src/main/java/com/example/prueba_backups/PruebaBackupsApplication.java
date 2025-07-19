3a273bc5583f711142beb88c471f2005960cb899
tpelegrina <theopelegrina468@gmail.com>
2025-07-19 20:51:11 -0300

    probando backup en produccion - railway

--- src/main/java/com/example/prueba_backups/PruebaBackupsApplication.java
+++ src/main/java/com/example/prueba_backups/PruebaBackupsApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class PruebaBackupsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("🔧 DB URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("America/Argentina/Buenos_Aires")));
 		SpringApplication.run(PruebaBackupsApplication.class, args);
 	}
 
